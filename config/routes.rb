e22a3123012aac30919036904eb48089eb1407d3
tyientyien3 <kunchikitaforp@gmai.com>
2020-01-25 04:52:31 +0000

    add: make devise contorollers and views.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,20 @@
 Rails.application.routes.draw do
-  devise_for :admin_users
-  devise_for :end_users
+  # end＿user, admin＿users の devise の routing
+  devise_for :admin_users, controllers: {
+    sessions:      'admin_users/sessions',
+    passwords:     'admin_users/passwords',
+    registrations: 'admin_users/registrations'
+  }
+  devise_for :end_users, controllers: {
+    sessions:      'end_users/sessions',
+    passwords:     'end_users/passwords',
+    registrations: 'end_users/registrations'
+  }
+
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
 end
