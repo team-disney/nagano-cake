21b72d7791b4d59eea47080c083ada171b8a7a52
Cray <craymaru@gmail.com>
2020-01-27 18:19:01 +0900

    Fix: Fix column names.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_25_114702) do
+ActiveRecord::Schema.define(version: 2020_01_27_091213) do
 
   create_table "addresses", force: :cascade do |t|
     t.string "name", null: false
@@ -64,15 +64,15 @@ ActiveRecord::Schema.define(version: 2020_01_25_114702) do
 
   create_table "genres", force: :cascade do |t|
     t.string "name", null: false
-    t.integer "genres_status", null: false
+    t.integer "status", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "items", force: :cascade do |t|
     t.string "name", null: false
-    t.integer "genres_id", null: false
-    t.integer "item_status", null: false
+    t.integer "genre_id", null: false
+    t.integer "status", null: false
     t.text "caption", null: false
     t.integer "price", null: false
     t.string "image_id", null: false
@@ -95,7 +95,7 @@ ActiveRecord::Schema.define(version: 2020_01_25_114702) do
     t.string "name", null: false
     t.string "zipcode", null: false
     t.string "address", null: false
-    t.integer "order_status", null: false
+    t.integer "status", null: false
     t.integer "sum_price", null: false
     t.integer "shipping_price", null: false
     t.integer "payment_price", null: false
