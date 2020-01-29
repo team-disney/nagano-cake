a195d4c91a978ddacfa2a25c35f82dc6cf3dd50c
Cray <craymaru@gmail.com>
2020-01-30 01:46:57 +0900

    Add: genre controller and seeds

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,6 +1,12 @@
 class GenresController < ApplicationController
   def show
+    # IS THIS PAGE GENRE ID?
     @genre = Genre.find(params[:id])
-    @filtered_items = @genre.item
+
+    # ONLY FILTERED ITEMS BY GENRE
+    @filtered_items = Item.where(genre_id: @genre)
+
+    # ONLY ACTIVE-GENRES
+    @active_genres = Genre.where(status: 1)
   end
 end
