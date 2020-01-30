d239a1f7a0355ee02baeb2a9c6c30a61ff524536
craymaru <56500094+craymaru@users.noreply.github.com>
2020-01-30 16:51:14 +0900

    Update end_user.rb

--- app/models/end_user.rb
+++ app/models/end_user.rb
@@ -1,8 +1,8 @@
 class EndUser < ApplicationRecord
+  # ASSOCIATIONS
+  has_many :addresses
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
-  has_many :addresses
 end
