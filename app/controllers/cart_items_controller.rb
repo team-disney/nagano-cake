bc328b6860ef47d018b3723e66b8407bff68653d
eriko7125 <t.eriko7125@gmail.com>
2020-02-03 07:12:14 +0000

    Fix: cart_item controller & views

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -4,31 +4,35 @@ class CartItemsController < ApplicationController
     @current_user_cart_items = CartItem.where(end_user_id: current_end_user.id)
     @cart_items = CartItem.all
     
-    @sum_price = 0
+    @sub_total = 0
     @current_user_cart_items.each do |cart_item|
-      @sum_price += cart_item.item.price * cart_item.amount
+      @sub_total += cart_item.item.price * cart_item.amount
     end
-
   end
 
-  # helper_method :current_cart
-
-  # def current_cart
-  #   if session[:end_user_id]
-  #     @cart = Item.find(session[:end_user_id])
-  #   else
-  #     render root_path
-  #   end
-  # end
-
   def create
-    @cart_item = CartItem.new(cart_item_params)
-    if @cart_item.save
-      redirect_to input_path(@cart_items)
-    else
-      @cart_item = CartItem.all
-      render action: :index
-    end
+    # @cart_item = CartItem.new(cart_item_params)
+    # if @cart_item.save
+    #   redirect_to cart_item_input_path(@cart_items)
+    # else
+    #   @cart_item = CartItem.all
+    #   render action: :index
+    # end
+
+    @order = Order.new(order_params)
+      if @order.save
+        redirect_to cart_item_thanks_path(@cart_items)
+      else
+        render action: :input
+      end
+
+    # @address = Address.new(address_params)
+    #   if @address.save
+    #     redirect_to cart_item_display_path(@cart_items)
+    #   else
+    #     render action: :input
+    #   end
+
   end
 
   def update
@@ -50,9 +54,20 @@ class CartItemsController < ApplicationController
   end
 
   def input
+    @order = Order.new
+    @address = Address.new
+    
   end
 
   def display
+    @current_user_cart_items = CartItem.where(end_user_id: current_end_user.id)
+    @cart_items = CartItem.all
+    @order = Order.new
+    @shipping_price = 800
+    @sub_total = 0
+    @current_user_cart_items.each do |cart_item|
+      @sub_total += cart_item.item.price * cart_item.amount
+    end
   end
 
   def thanks
@@ -66,5 +81,13 @@ class CartItemsController < ApplicationController
 
   def cart_item_params
     params.require(:cart_item).permit(:end_user_id, :item_id, :amount)
+  end
+
+  def order_params
+    params.require(:order).permit(:payment_method,:address,:cart_item_id)
+  end
+
+  def address_params
+    params.require(:address).permit(:address, :name, :zipcode)
   end
 end
