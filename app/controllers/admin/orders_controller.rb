d6ea72da2efdb6e2b9be28e19e57bc160f003fa5
tyientyien3 <kunchikitaforp@gmai.com>
2020-02-04 04:14:44 +0000

    Add: admin homes controller.

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,42 +1,47 @@
 class Admin::OrdersController < ApplicationController
-    def index
-        @orders = Order.all
-        @order_details = OrderDetail.all
-        
-        @filtered_orders = Order.where(order_id: @Order)
+  def index
+      @orders = Order.all
+      @order_details = OrderDetail.all
+      
+      @filtered_orders = Order.where(order_id: @Order)
+  end
 
-    end
+  # 本日の注文のみ全表示
+  def index_today
+    @orders = Order.all
+    @orders = Order.where(created_at: (Time.now.midnight - 1.day)..Time.now.midnight)
+  end
 
-    def update
-        @order = Order.find(params[:id])
-        if @order.update(order_params)
-            redirect_to admin_order_path(@order)
-        else
-            render :show
-        end
+  def update
+      @order = Order.find(params[:id])
+      if @order.update(order_params)
+          redirect_to admin_order_path(@order)
+      else
+          render :show
+      end
 
-        # @order_detail = OrderDetail.find(params[:id])
-        # if @order_detail.update(order_detail_params)
-        #     redirect_to admin_order_path(@order_detail), notice: "製作ステータスを更新しました！"
-        # else
-        #     render :show
-        # end
-    
-    end
+      # @order_detail = OrderDetail.find(params[:id])
+      # if @order_detail.update(order_detail_params)
+      #     redirect_to admin_order_path(@order_detail), notice: "製作ステータスを更新しました！"
+      # else
+      #     render :show
+      # end
+  
+  end
 
-    def show
-        @order = Order.find(params[:id])
-        @order_details = @order.order_details
-    end
+  def show
+      @order = Order.find(params[:id])
+      @order_details = @order.order_details
+  end
 
-    private
-    def order_params
-        params.require(:order).permit(:status)
-        # params.require(:order).permit(:status, order_detail_attributes: [:production_status])
-        # accepts_nested_attributes_for :order_details
-    end
+  private
+  def order_params
+      params.require(:order).permit(:status)
+      # params.require(:order).permit(:status, order_detail_attributes: [:production_status])
+      # accepts_nested_attributes_for :order_details
+  end
 
-    # def order_detail_params
-    #     params.require(:order_detail).permit(:production_status)
-    # end
+  # def order_detail_params
+  #     params.require(:order_detail).permit(:production_status)
+  # end
 end
